47bc6cb7e6f7eba9396a39244adcda6f62c1f185
chuigda <icey@icey.tech>
2025-01-29 06:34:27 +0800

    传统手艺

---
+++ include/lex.h
@@ -0,0 +1,73 @@
+#ifndef P6K14_LEX_H
+#define P6K14_LEX_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum {
+    PL9_TK_Let       = 0,
+    PL9_TK_Native    = 1,
+    PL9_TK_Rec       = 2,
+    PL9_TK_In        = 3,
+    PL9_TK_If        = 4,
+    PL9_TK_Then      = 5,
+    PL9_TK_Else      = 6,
+    PL9_TK_Loop      = 7,
+    PL9_TK_Break     = 8,
+    PL9_TK_Continue  = 9,
+    PL9_TK_Return    = 10,
+
+    PL9_TK_And       = 11,
+    PL9_TK_Or        = 12,
+    PL9_TK_Not       = 13,
+    PL9_TK_Is        = 14,
+    PL9_TK_True      = 15,
+    PL9_TK_False     = 16,
+    PL9_TK_Nil       = 17,
+
+    PL9_TK_LParen    = 18,
+    PL9_TK_RParen    = 19,
+    PL9_TK_Arrow     = 20,
+    PL9_TK_Comma     = 21,
+    PL9_TK_Lambda    = 22,
+    PL9_TK_DColon    = 23,
+    PL9_TK_Eq        = 24,
+    PL9_TK_ColonEq   = 25,
+    PL9_TK_Lt        = 26,
+    PL9_TK_Gt        = 27,
+    PL9_TK_LtEq      = 28,
+    PL9_TK_GtEq      = 29,
+    PL9_TK_DEq       = 30,
+    PL9_TK_NEq       = 31,
+    PL9_TK_Plus      = 32,
+    PL9_TK_Minus     = 33,
+    PL9_TK_Mul       = 34,
+    PL9_TK_Div       = 35,
+    PL9_TK_Mod       = 36,
+    PL9_TK_Tilde     = 37,
+    PL9_TK_Exclaim   = 38,
+    PL9_TK_Semicolon = 39,
+
+    PL9_TK_Int       = 40,
+    PL9_TK_Float     = 41,
+    PL9_TK_Char      = 42,
+    PL9_TK_String    = 43,
+
+    PL9_TK_EOF       = 44,
+    PL9_TK_Absent    = -1
+} PL9TokenKind;
+
+typedef struct {
+    PL9TokenKind k;
+    unsigned row;
+    unsigned col;
+    const char *valbeg;
+    const char *valend;
+} PL9Token;
+
+#ifdef __cplusplus
+}; /* extern "C" */
+#endif
+
+#endif /* P6K14_LEX_H */
--- include/syntax.h
+++ include/syntax.h
@@ -1,9 +1,122 @@
 #ifndef P6K14_SYNTAX_H
 #define P6K14_SYNTAX_H
 
+#include "lex.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+#define PL9_SYNTAX_NODE_COMMON \
+    struct stPL9SyntaxNode *next; \
+    int k;
+
+typedef struct stPL9SyntaxNode {
+    PL9_SYNTAX_NODE_COMMON
+} PL9SyntaxNode, PL9Program;
+
+typedef enum {
+    PL9_SK_LetDeclOnly = 0,
+    PL9_SK_LetDef = 1,
+
+    PL9_SK_LetIn = 2,
+    PL9_SK_Abs = 3,
+    PL9_SK_If = 4,
+    PL9_SK_Loop = 5,
+    PL9_SK_Break = 6,
+    PL9_SK_Continue = 7,
+    PL9_SK_Return = 8,
+    PL9_SK_Assign = 9,
+    PL9_SK_Binary = 10,
+    PL9_SK_Unary = 11,
+    PL9_SK_Call = 12,
+    PL9_SK_Ident = 13,
+    PL9_SK_Literal = 14,
+
+    PL9_SK_TypeVar = 15,
+    PL9_SK_TypeOp = 16,
+    PL9_SK_UnitType = 17,
+    PL9_SK_FuncType = 18
+} PL9SyntaxNodeKind;
+
+typedef struct stPL9LetDeclNode PL9LetDeclNode;
+    typedef struct stPL9LetDeclOnlyNode PL9LetDeclOnlyNode;
+    typedef struct stPL9LetDefNode PL9LetDefNode;
+typedef struct stPL9ExprNode PL9ExprNode;
+    typedef struct stPL9LetInNode PL9LetInNode;
+    typedef struct stPL9AbsNode PL9AbsNode;
+    typedef struct stPL9IfNode PL9IfNode;
+    typedef struct stPL9LoopNode PL9LoopNode;
+    typedef struct stPL9BreakNode PL9BreakNode;
+    typedef struct stPL9ContinueNode PL9ContinueNode;
+    typedef struct stPL9ReturnNode PL9ReturnNode;
+    typedef struct stPL9AssignNode PL9AssignNode;
+    typedef struct stPL9BinaryNode PL9BinaryNode;
+    typedef struct stPL9UnaryNode PL9UnaryNode;
+    typedef struct stPL9CallNode PL9CallNode;
+    typedef struct stPL9IdentNode PL9IdentNode;
+    typedef struct stPL9LiteralNode PL9LiteralNode;
+typedef struct stPL9TypeNode PL9TypeNode;
+    typedef struct stPL9TypeVarNode PL9TypeVarNode;
+    typedef struct stPL9TypeOpNode PL9TypeOpNode;
+    typedef struct stPL9UnitTypeNode PL9UnitTypeNode;
+    typedef struct stPL9FuncTypeNode PL9FuncTypeNode;
+
+#define PL9_LET_DECL_NODE_COMMON \
+    PL9_SYNTAX_NODE_COMMON \
+    PL9Token let; \
+    PL9Token ident;
+
+struct stPL9LetDeclNode {
+    PL9_LET_DECL_NODE_COMMON
+};
+
+struct stPL9LetDeclOnlyNode {
+    PL9_LET_DECL_NODE_COMMON
+
+    PL9Token native;
+    PL9Token dcolon;
+    PL9TypeNode *tnode;
+};
+
+struct stPL9LetDefNode {
+    PL9_LET_DECL_NODE_COMMON
+
+    PL9Token eq;
+    PL9ExprNode *expr;
+};
+
+struct stPL9TypeVarNode {
+    PL9_SYNTAX_NODE_COMMON
+
+    PL9Token ident;
+};
+
+struct stPL9TypeOpNode {
+    PL9_SYNTAX_NODE_COMMON
+
+    PL9Token op;
+    PL9Token lt;
+    PL9TypeNode *tlist;
+    PL9Token gt;
+};
+
+struct stPL9UnitTypeNode {
+    PL9_SYNTAX_NODE_COMMON
+
+    PL9Token lparen;
+    PL9Token rparen;
+};
+
+struct stPL9FuncTypeNode {
+    PL9_SYNTAX_NODE_COMMON
+
+    PL9Token lparen;
+    PL9TypeNode *tlist;
+    PL9Token rparen;
+    PL9Token arrow;
+    PL9TypeNode *tret;
+};
 
 #ifdef __cplusplus
 } /* extern "C" */
